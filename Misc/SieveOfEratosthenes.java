08346f610f082ab66d06a5b8c83900d4715cc1d9
Aswin G <aswinganesh666@gmail.com>
2019-07-10 01:25:15 +0530

    Add Sieve of Eratosthenes

---
+++ Misc/SieveOfEratosthenes.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class SieveOfEratosthenes {
+    static ArrayList<Integer> generatePrimes(int max) {
+        boolean flags[] = new boolean[max + 1];
+        flags[0] = false;
+        flags[1] = false;
+        for (int i = 2; i <= max; i++)
+            flags[i] = true;
+        int curr = 2;
+        while (curr < Math.sqrt(max)) {
+            if (flags[curr])
+                for (int i = curr * curr; i <= max; i += curr)
+                    flags[i] = false;
+            curr++;
+        }
+        ArrayList<Integer> ans = new ArrayList<>();
+        for (int i = 0; i <= max; i++)
+            if (flags[i])
+                ans.add(i);
+        return ans;
+    }
+
+    public static void main(String args[]) {
+        System.out.println("Enter limit till which to generate primes");
+        Scanner in = new Scanner(System.in);
+        int max = in.nextInt();
+        ArrayList<Integer> primes = generatePrimes(max);
+        System.out.println(primes.toString());
+    }
+}
