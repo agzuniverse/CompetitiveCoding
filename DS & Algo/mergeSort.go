9f37b57c40a652a8ba606c9d1a9a3ffa368e2766
Aswin G <aswinganesh666@gmail.com>
2019-08-03 00:08:11 +0530

    Take user input instead of predefined input

--- DS & Algo/mergeSort.go
+++ DS & Algo/mergeSort.go
@@ -1,9 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	a := []int{5, 10, 8, 9, 0, 2, 7, 28, 1, 3}
+	// Example a := []int{5, 10, 8, 9, 0, 2, 7, 28, 1, 3}
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println("Enter array to be sorted as space-separated integers")
+	scanner.Scan()
+	a := make([]int, 0)
+	for _, v := range strings.Split(scanner.Text(), " ") {
+		i, _ := strconv.Atoi(v)
+		a = append(a, i)
+	}
 	fmt.Println(a)
 	mergeSort(a, 0, len(a)-1)
 	fmt.Println(a)
