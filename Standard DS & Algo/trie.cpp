32908e2bf948a5053d05d28d21fa7e8d834e623f
Aswin G <aswinganesh666@gmail.com>
2020-08-06 22:02:31 +0530

    Add deletion of word from trie

--- Standard DS & Algo/trie.cpp
+++ Standard DS & Algo/trie.cpp
@@ -6,6 +6,7 @@ class TrieNode
 {
 public:
     bool isTerminating;
+    int noOfChildren;
     TrieNode *children[26];
 };
 
@@ -13,10 +14,31 @@ class Trie
 {
     TrieNode *root;
 
+    bool deleteWordRecursive(TrieNode *t, string s, int i)
+    {
+        if (i == s.length())
+            return true;
+        bool hasChildBeenDeleted = deleteWordRecursive(t->children[s[i] - 'a'], s, i + 1);
+        if (hasChildBeenDeleted)
+        {
+            t->children[s[i] - 'a'] = nullptr;
+            t->noOfChildren--;
+        }
+        if (i == s.length() - 1)
+            t->isTerminating = false;
+        if (t->noOfChildren == 0 && t->isTerminating == false)
+        {
+            delete t;
+            return true;
+        }
+        return false;
+    }
+
 public:
     Trie()
     {
         root = new TrieNode();
+        root->noOfChildren = 0;
         for (int i = 0; i < 26; i++)
         {
             root->children[i] = nullptr;
@@ -31,13 +53,14 @@ public:
             if (temp->children[word[i] - 'a'] == nullptr)
             {
                 temp->children[word[i] - 'a'] = new TrieNode();
+                temp->noOfChildren++;
             }
             temp = temp->children[word[i] - 'a'];
         }
         temp->isTerminating = true;
     }
 
-    void searchWord(string word)
+    bool searchWord(string word)
     {
         TrieNode *temp = root;
         for (int i = 0; i < word.length(); i++)
@@ -45,16 +68,28 @@ public:
             if (temp->children[word[i] - 'a'] == nullptr)
             {
                 cout << "Word not found\n";
-                return;
+                return false;
             }
             temp = temp->children[word[i] - 'a'];
         }
         if (temp->isTerminating)
         {
             cout << "Word found!\n";
+            return true;
         }
         else
             cout << "Word not found\n";
+        return false;
+    }
+
+    void deleteWord(string word)
+    {
+        if (searchWord(word))
+        {
+            deleteWordRecursive(root, word, 0);
+            cout << "Word deleted\n";
+            return;
+        }
     }
 };
 
@@ -63,6 +98,17 @@ int main()
     Trie t;
     t.addWord("hello");
     t.addWord("search");
-    t.searchWord("hel");
+
+    t.searchWord("hel"); // word does not exist
     t.searchWord("search");
+
+    t.deleteWord("search");
+    t.searchWord("search"); // word does not exist
+
+    t.deleteWord("hel"); // word does not exist, delete should not do anything
+
+    t.addWord("hel");
+    t.deleteWord("hello");
+
+    t.searchWord("hel"); // Should exist despite hello being deleted.
 }
